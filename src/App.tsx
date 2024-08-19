de4a30ce60027b94e0c35d2cc831998971aa442c
EonMcC <iain2eyes@gmail.com>
2024-08-19 11:32:08 +0100

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import './App.scss';
 function App() {
   return (
     <div className="app">
-
+      <header>
+        <h1>Iain McClafferty</h1>
+        <h2>Developer | Designer | Storyteller</h2>
+      </header>
     </div>
   );
 }
